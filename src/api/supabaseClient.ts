4dfd30e0867b84ad1cd7b27b1e98b13c8123836f
leliquideur <le.liquideur01@gmail.com>
2025-01-04 22:35:41 +0100

    Mise à jour de la configuration Supabase pour utiliser des variables d'environnement et ajout du hook useAuth pour la gestion de l'authentification.

--- src/api/supabaseClient.ts
+++ src/api/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://tqkuwuaejndsaxlcmvfx.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxa3V3dWFlam5kc2F4bGNtdmZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ3NzcxMTksImV4cCI6MjA1MDM1MzExOX0.FsXaU08KgIjlQ_HAUL-mt3AlSJspy1GKcto8XNE0GeY";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
